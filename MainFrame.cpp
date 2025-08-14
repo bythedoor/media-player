6c76f0f573bca892c63b3d324158ab469f1d8b11
bythedoor <bythedoor115@gmail.com>
2025-08-14 23:59:46 +0200

    adding buttons

--- MainFrame.cpp
+++ MainFrame.cpp
@@ -3,5 +3,7 @@
 
 MainFrame::MainFrame(const wxString& title): wxFrame(nullptr, wxID_ANY, title)
 {
-    
+    wxPanel* panel = new wxPanel(this); // creating panel for buttons and text
+    wxButton* playButton = new wxButton(this, wxID_ANY, "play !", wxPoint(150, 50), wxSize(100, 35));
+    wxButton* pauseButton = new wxButton(this, wxID_ANY, "pause...", wxPoint(150, 90), wxSize(100, 35));
 }
