8b2d88b91d5d17330b61e14562251d404a814c4a
bythedoor <bythedoor115@gmail.com>
2025-08-15 18:29:49 +0200

    adding extra buttons + text

--- MainFrame.cpp
+++ MainFrame.cpp
@@ -1,10 +1,20 @@
 #include "MainFrame.h"
 #include "wx/gdicmn.h"
+#include "wx/gtk/button.h"
+#include "wx/gtk/stattext.h"
 #include <wx/wx.h>
 
 MainFrame::MainFrame(const wxString& title): wxFrame(nullptr, wxID_ANY, title, wxDefaultPosition, wxSize(800,600)) 
 {
     wxPanel* panel = new wxPanel(this); // creating panel for buttons and text
-    wxButton* playButton = new wxButton(panel, wxID_ANY, "play !", wxPoint(250, 500), wxSize(100, 35));
-    wxButton* pauseButton = new wxButton(panel, wxID_ANY, "pause...", wxPoint(450, 500), wxSize(100, 35));
+
+    //text for displaying the music
+    wxStaticText* songText = new wxStaticText(panel, wxID_ANY, "nct wish - steady", wxPoint(335, 30));
+
+    //buttons for controlling the music
+    wxButton* playButton = new wxButton(panel, wxID_ANY, "play !", wxPoint(275, 500), wxSize(100, 35));
+    wxButton* pauseButton = new wxButton(panel, wxID_ANY, "pause...", wxPoint(425, 500), wxSize(100, 35));
+
+    wxButton* previousButton = new wxButton(panel, wxID_ANY, "previous", wxPoint(150, 500), wxSize(100, 35));
+    wxButton* nextButton = new wxButton(panel, wxID_ANY, "next", wxPoint(550, 500), wxSize(100, 35));
 }
--- README.md
+++ README.md
@@ -3,7 +3,9 @@ a cute media player :)
 
 ## to do list ⋆˙⟡.ೃ࿔⋆
 - [x] fixing compiling problems with C++
-- [ ] add buttons (pause/continue, previous/next,...)
+- [x] add buttons (pause/continue, previous/next,...)
+- [x] add text of metadata (name of artist, name of the song,...)
+- [ ] make buttons play or stop a song
+- [ ] fetch local music files
 - [ ] display photo of the media playing
-- [ ] add text of metadata (name of artist, name of the song,...)
 - [ ] add a progress bar
